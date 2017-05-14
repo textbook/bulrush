c0cc63cc5a3b09b892c869b0ac4a378f2a75c600
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2017-05-14 15:56:11 +0100

    Make Bulrush a Python module

---
+++ bulrush.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+# https://github.com/getpelican/pelican/issues/1564#issuecomment-282136049
+PATH = str(Path(__file__).parent)
+"""The path to the Bulrush theme directory."""
