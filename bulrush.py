d0b49303deac835049ae9bf7a16950b989e9b903
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2017-05-14 16:36:03 +0100

    Update .gitignore and remove redundant file

--- bulrush.py
+++
@@ -1,5 +0,0 @@
-from pathlib import Path
-
-# https://github.com/getpelican/pelican/issues/1564#issuecomment-282136049
-PATH = str(Path(__file__).parent)
-"""The path to the Bulrush theme directory."""
